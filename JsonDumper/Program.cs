ba992428f5892e1a9ece269ef2cbf08e43e801e4
Adam ? <adamznow@gmail.com>
2021-10-15 21:34:20 -0700

    Now parses backwards if json files already exist

--- JsonDumper/Program.cs
+++ JsonDumper/Program.cs
@@ -9,6 +9,19 @@ namespace JsonDumper
 {
     internal class Program
     {
+        private static JsonSerializer serialization = new JsonSerializer()
+        {
+            Converters =
+                {
+                new CheckThingConverter(),
+                    new FloatSourceConverter(),
+                    new JumpConverter(),
+                    new ObjectSourceConverter(),
+                    new StateActionConverter(),
+                    new Vector3Converter()
+                },
+        };
+
         private static void Write(string dst, string fullPath)
         {
             var watch = new Stopwatch();
@@ -24,18 +37,6 @@ namespace JsonDumper
             watch.Stop();
             Console.WriteLine($"Creation of type structure took: {watch.Elapsed}");
             watch.Reset();
-            var serialization = new JsonSerializer()
-            {
-                Converters =
-                {
-                    new CheckThingConverter(),
-                    new FloatSourceConverter(),
-                    new JumpConverter(),
-                    new ObjectSourceConverter(),
-                    new StateActionConverter(),
-                    new Vector3Converter()
-                },
-            };
             Console.WriteLine("Json dump...");
             var outpFile = Path.GetFileNameWithoutExtension(fullPath);
             outpFile = Path.Combine(dst, outpFile) + ".json";
@@ -51,18 +52,6 @@ namespace JsonDumper
 
         private static void Read(string path)
         {
-            var serialization = new JsonSerializer()
-            {
-                Converters =
-                {
-                    new CheckThingConverter(),
-                    new FloatSourceConverter(),
-                    new JumpConverter(),
-                    new ObjectSourceConverter(),
-                    new StateActionConverter(),
-                    new Vector3Converter()
-                },
-            };
             var watch = new Stopwatch();
             watch.Start();
             var data = new JsonTextReader(File.OpenText(path));
@@ -82,6 +71,29 @@ namespace JsonDumper
             Console.WriteLine($"Creation of JSON took: {watch.Elapsed}");
         }
 
+        private static void ReadWrite(string fullPathJson, string dstFolder)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            var data = new JsonTextReader(File.OpenText(fullPathJson));
+            var dataToWrite = serialization.Deserialize<SerialMoveset>(data);
+            watch.Stop();
+            Console.WriteLine($"Deserialization took: {watch.Elapsed}");
+            var outpFile = Path.Combine(dstFolder, Path.GetFileNameWithoutExtension(fullPathJson)) + "_new.txt";
+            if (File.Exists(outpFile))
+                File.Delete(outpFile);
+            watch.Reset();
+            var writer = new BulkSerializeWriter();
+            watch.Start();
+            dataToWrite.Write(writer);
+            using var fs = File.OpenWrite(outpFile);
+            using var dsts = new StreamWriter(fs);
+            writer.Serialize(dsts);
+            watch.Stop();
+
+            Console.WriteLine($"Writeout took: {watch.Elapsed}");
+        }
+
         private static void Main(string[] args)
         {
             Console.WriteLine("Enter path to exported TextAsset data...");
@@ -94,29 +106,31 @@ namespace JsonDumper
             var dst = "output";
             if (Directory.Exists(dst))
             {
-                Directory.Delete(dst, true);
-            }
-            Directory.CreateDirectory(dst);
-            if (Directory.Exists(p))
-            {
-                foreach (var path in Directory.EnumerateFiles(p))
-                {
-                    Write(dst, path);
-                }
-                Console.WriteLine("Success!");
-            }
-            else if (File.Exists(p))
-            {
-                Write(dst, p);
-                Console.WriteLine("Success!");
-            }
-            if (Directory.Exists(dst))
-            {
+                var dstOut = Path.Combine(dst, "text_data");
+                if (!Directory.Exists(dstOut))
+                    Directory.CreateDirectory(dstOut);
                 foreach (var path in Directory.EnumerateFiles(dst))
                 {
-                    Read(path);
+                    ReadWrite(path, dstOut);
                 }
-                Console.WriteLine("Success Reading!");
+                Console.WriteLine("Success Reading/Writing!");
+            }
+            else
+            {
+                Directory.CreateDirectory(dst);
+                if (Directory.Exists(p))
+                {
+                    foreach (var path in Directory.EnumerateFiles(p))
+                    {
+                        Write(dst, path);
+                    }
+                    Console.WriteLine("Success!");
+                }
+                else if (File.Exists(p))
+                {
+                    Write(dst, p);
+                    Console.WriteLine("Success!");
+                }
             }
         }
     }
