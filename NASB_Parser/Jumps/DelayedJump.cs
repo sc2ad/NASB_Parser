76299b769238bd08c42ec3a67819d045f464fc3b
Megalon <megalongaming@gmail.com>
2021-12-09 16:35:44 -0800

    Add DelayedJump

---
+++ NASB_Parser/Jumps/DelayedJump.cs
@@ -0,0 +1,30 @@
+﻿using NASB_Parser.FloatSources;
+
+namespace NASB_Parser.Jumps
+{
+    class DelayedJump : Jump
+    {
+		public FloatSource Height { get; set; }
+		public FloatSource AutoHoldFrames { get; set; }
+		public FloatSource YVelMaxOnRelease { get; set; }
+
+		public DelayedJump()
+        {
+		}
+
+		internal DelayedJump(BulkSerializeReader reader) : base(reader)
+		{
+			Height = FloatSource.Read(reader);
+			AutoHoldFrames = FloatSource.Read(reader);
+			YVelMaxOnRelease = FloatSource.Read(reader);
+		}
+
+		public override void Write(BulkSerializeWriter writer)
+		{
+			base.Write(writer);
+			writer.Write(Height);
+			writer.Write(AutoHoldFrames);
+			writer.Write(YVelMaxOnRelease);
+		}
+	}
+}
