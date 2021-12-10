9b0a5e616a10f621a92829f846dbe95d607ca7ff
Megalon <megalongaming@gmail.com>
2021-12-09 16:28:29 -0800

    Add KnockbackAltJump

---
+++ NASB_Parser/Jumps/KnockbackAltJump.cs
@@ -0,0 +1,45 @@
+﻿using NASB_Parser.FloatSources;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NASB_Parser.Jumps
+{
+    class KnockbackAltJump : Jump
+    {
+        public FloatSource XDir { get; set; }
+        public FloatSource YDir { get; set; }
+        public FloatSource LaunchDist { get; set; }
+        public FloatSource Frames { get; set; }
+        public FloatSource DiType { get; set; }
+        public FloatSource DiAngleIn { get; set; }
+        public FloatSource DiAngleOut { get; set; }
+
+        public KnockbackAltJump()
+        {
+        }
+
+        internal KnockbackAltJump(BulkSerializeReader reader) : base(reader)
+        {
+            XDir = FloatSource.Read(reader);
+            YDir = FloatSource.Read(reader);
+            LaunchDist = FloatSource.Read(reader);
+            Frames = FloatSource.Read(reader);
+            DiType = FloatSource.Read(reader);
+            DiAngleIn = FloatSource.Read(reader);
+            DiAngleOut = FloatSource.Read(reader);
+        }
+
+        public override void Write(BulkSerializeWriter writer)
+        {
+            base.Write(writer);
+            writer.Write(XDir);
+            writer.Write(YDir);
+            writer.Write(LaunchDist);
+            writer.Write(Frames);
+            writer.Write(DiType);
+            writer.Write(DiAngleIn);
+            writer.Write(DiAngleOut);
+        }
+    }
+}
