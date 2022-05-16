4db018a35d42ac907b6ebfaf7c1042134a5d31be
Megalon <megalongaming@gmail.com>
2022-05-16 15:47:24 -0700

    Update SAStopJump
    Build 26995

--- NASB_Parser/StateActions/SAStopJump.cs
+++ NASB_Parser/StateActions/SAStopJump.cs
@@ -8,15 +8,27 @@ namespace NASB_Parser.StateActions
     {
         public bool StopAll { get; set; }
         public string JumpId { get; set; }
+        public string[] JumpIds { get; set; }
 
         public SAStopJump()
-        {
-        }
+        { }
 
         internal SAStopJump(BulkSerializeReader reader) : base(reader)
         {
             StopAll = reader.ReadBool();
             JumpId = reader.ReadString();
+            
+            if (Version > 0)
+            {
+                int numJumpIds = reader.ReadInt();
+                for (int i = 0; i < numJumpIds; i++)
+                {
+                    JumpIds[i] = reader.ReadString();
+                }
+            } else
+            {
+                JumpIds = new string[0];
+            }
         }
 
         public override void Write(BulkSerializeWriter writer)
@@ -24,6 +36,11 @@ namespace NASB_Parser.StateActions
             base.Write(writer);
             writer.Write(StopAll);
             writer.Write(JumpId);
+            writer.Write(JumpId.Length);
+            for (int i = 0; i < JumpIds.Length; i++)
+            {
+                writer.Write(JumpIds[i]);
+            }
         }
     }
 }
